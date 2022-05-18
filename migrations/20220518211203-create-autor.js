b8862d5facefa798e453c2246fc1202ccb1f4f43
Ana Reis <86137114+dorazzi@users.noreply.github.com>
2022-05-18 21:29:36 +0000

    Initial commit

---
+++ migrations/20220518211203-create-autor.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('autors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+          allowNull: false,
+        type: Sequelize.STRING
+      },
+      sobrenome: {
+          allowNull: false,
+        type: Sequelize.STRING
+      },
+      data_nascimento: {
+          allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('autors');
+  }
+};
